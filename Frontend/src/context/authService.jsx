94512f3a8b91510d46318480285047d1d0ffb3f5
Aniket Dutta <114622119+anikkkettt@users.noreply.github.com>
2025-03-20 18:26:49 +0530

    Update authService.jsx

--- Frontend/src/context/authService.jsx
+++ Frontend/src/context/authService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const client = axios.create({
-    baseURL: `http://localhost:8000/api/users`
+    baseURL: `https://letsmeet-wloh.onrender.com/api/users`
 });
 
 export const register = async (name, username, password) => {
